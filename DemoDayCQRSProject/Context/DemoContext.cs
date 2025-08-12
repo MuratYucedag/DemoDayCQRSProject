3c592c537367ab1e33cb82bf60cddfce443ae209
MuratYucedag <yucedagmurat23@gmail.com>
2025-08-12 20:09:33 +0300

    Add project files.

---
+++ DemoDayCQRSProject/Context/DemoContext.cs
@@ -0,0 +1,15 @@
+﻿using DemoDayCQRSProject.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoDayCQRSProject.Context
+{
+    public class DemoContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-R7AR1ND;initial catalog=DemoCQRSDb;integrated Security=true;trust server certificate=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
