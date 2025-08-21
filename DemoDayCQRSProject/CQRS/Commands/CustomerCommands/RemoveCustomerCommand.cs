cb08e8b4a424a7c1b9c421427ea4112af63b9cb5
MuratYucedag <yucedagmurat23@gmail.com>
2025-08-21 16:16:07 +0300

    ...

---
+++ DemoDayCQRSProject/CQRS/Commands/CustomerCommands/RemoveCustomerCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoDayCQRSProject.CQRS.Commands.CustomerCommands
+{
+    public class RemoveCustomerCommand
+    {
+        public int CustomerId { get; set; }
+        public RemoveCustomerCommand(int customerId)
+        {
+            CustomerId = customerId;
+        }
+    }
+}
